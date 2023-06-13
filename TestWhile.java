2eb5dbaa462da9a4fb8fddacaed7fe350fa0a4e9
liyanxin8023 <liyanxin8023@gmail.com>
2023-06-13 20:16:15 +0900

    self

---
+++ TestWhile.java
@@ -0,0 +1,11 @@
+public class TestWhile {
+	public static void main(String[] args) {
+		int x = 20;
+		
+		do {
+			System.out.println("x的值是" + x);
+			x++;
+			System.out.println("\n");
+			} while(x < 20) ;	
+		}
+	}
